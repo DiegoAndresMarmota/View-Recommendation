ab38a11102316c25a10740c1d9e8e8c5892472a0
Diego Andres <diegoechalarp@hotmail.com>
2022-10-09 16:55:54 -0300

    Lógica de App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Button from './components/Button';
 import './styles/App.css';
 import './styles/index.css';
+import './styles/button.css';
 import Carta from './components/Carta';
 import Gato1 from './img/gato1.jpg';
 import Gato2 from './img/gato2.jpg';
@@ -10,16 +11,27 @@ import Gato3 from './img/gato3.jpg';
 
 function App() {
 
-  const [modo, setModo] = useState('vistaDia');
+  const [modo, setModo] = useState('dia');
+
+  function cambiarModo(){
+    if(modo == 'dia') {
+      setModo('noche')
+    }else{
+      setModo('dia')
+    }
+  }
 
   return (
-    <div className='container-principal'>
+    <div className={'container-principal ' + modo}>
+      <Button
+        cambiarModo={cambiarModo}
+        modoVista={modo == 'noche' ? 'dia' : 'noche'}
+      />
       <Carta
         imagen={Gato1}
         nombre="Mr. Galleta"
         calificacion="⚡⚡⚡⚡"
         lorem="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, distinctio?"
-
       />
       <Carta
         imagen={Gato2}
