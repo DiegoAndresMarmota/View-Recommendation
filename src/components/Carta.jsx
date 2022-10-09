2b33174a9440fef3beb647d03fcf07b830623529
Diego Andres <diegoechalarp@hotmail.com>
2022-10-09 14:28:47 -0300

    Actualización del Grid

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function Carta({imagen, nombre, calificacion, lorem}) {
+function Carta({ imagen, nombre, calificacion, lorem }) {
     return (
         <div className='carta-container'>
             <div>
                 <img className='carta-img' src={imagen} alt="carta-imagen-perfil" />
                 <h3 className='carta-nombre'>{nombre}</h3>
+            </div>
+            <div className='carta-contenido'>
+                <p className='carta-lorem'>{lorem}</p>
                 <p className='carta-calificacion'>
                     {calificacion}
                 </p>
-            </div>
-            <div>
-                <p className='carta-lorem'>{lorem}</p>
                 <button className='carta-valoracion'>
                     <i class="fa-solid fa-thumbs-up"></i>
                 </button>
