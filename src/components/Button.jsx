82266d6f7a44d6b33385ab9f9f8f4a5f76c8ced0
Diego Andres <diegoechalarp@hotmail.com>
2022-10-09 16:55:29 -0300

    Estilos de button.css

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,4 @@
+
 function Button({ cambiarModo, modoVista }) {
     //A través del evento onClick, se llama la función cambiarModo al presionar el button, entregara un valor modoVista, el cual ira cambiando como props
     return (
